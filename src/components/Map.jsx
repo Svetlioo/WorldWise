6d9f1eda76dd1bd78a80f8c22c38a19078ab99db
Svetoslav Iliev <svetlio.iliev17@gmail.com>
2023-08-29 20:45:19 +0300

    Dynamic routing with params and queries

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,22 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  console.log(searchParams);
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <h2>
+        Position: {lat}, {lng}
+      </h2>
+      <button onClick={() => setSearchParams({ lat: 23, lng: 50 })}>
+        change position
+      </button>
+    </div>
+  );
 }
 
 export default Map;
